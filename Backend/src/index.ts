6af753c056927c29ffde1239709f9fa7aad72081
Ben <trannhatminh2006vl@gmail.com>
2026-05-10 14:28:49 +0700

    implement apm with site24x7

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,5 @@
-// import('apminsight')
-//   .then(({ default: AgentAPI }) => AgentAPI.config())
-//   .catch(() => console.log('APM not available in this environment'));
+import AgentAPI from "apminsight";
+AgentAPI.config();
 
 import "dotenv/config";
 import cors from "cors";
